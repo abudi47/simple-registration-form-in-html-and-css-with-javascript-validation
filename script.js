fefc2060ddfc03c5eb9bc8a545275ebfeb46d8c8
Abudi <abduselamt47@gmail.com>
2025-03-24 22:20:52 +0300

    sc mid

--- script.js
+++ script.js
@@ -3,8 +3,8 @@
 const form = document.getElementById("form");
 const username = document.getElementById("username");
 const email = document.getElementById("email");
-const password = document.getElementById("password");
 const password2 = document.getElementById("password2");
+const password = document.getElementById("password");
 
 form.addEventListener('submit', e => {
     e.preventDefault();
